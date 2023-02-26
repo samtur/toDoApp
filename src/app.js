2eca488d83b96f2ef19dd56c87f41140c3343dc8
samtur <108389765+samtur@users.noreply.github.com>
2023-02-26 00:51:08 +0000

    Styled form

--- src/app.js
+++ src/app.js
@@ -2,11 +2,8 @@ const appFunc = () => {
   const addbtn = document.querySelector(".contentTxt");
   addbtn.addEventListener("click", function () {
     const formContainer = document.querySelector(".taskFormContainer");
-    if (formContainer.classList.contains("hidden")) {
-      formContainer.classList.remove("hidden");
-    } else {
-      formContainer.classList.add("hidden");
-    }
+    addbtn.classList.add("hidden");
+    formContainer.classList.remove("hidden");
   });
 };
 
